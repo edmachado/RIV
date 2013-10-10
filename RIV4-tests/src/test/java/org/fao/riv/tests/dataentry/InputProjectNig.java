97f187cad281071fa509ca94710309eeb36ff9ba
Bar Zecharya <bar.zecharya@fao.org>
2013-10-10 13:18:38 +0000

    new svn location;
    git-svn-id: https://svn.fao.org/projects/ruralinvest/RIV4/trunk@66590 d7fa552c-c32d-11e1-9f2d-6d39f2a56844

---
+++ RIV4-tests/src/test/java/org/fao/riv/tests/dataentry/InputProjectNig.java
@@ -0,0 +1,309 @@
+package org.fao.riv.tests.dataentry;
+
+import static net.sourceforge.jwebunit.junit.JWebUnit.*;
+
+import java.util.concurrent.Callable;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import org.fao.riv.tests.utils.InputParam.InputParamType;
+import org.fao.riv.tests.utils.TestTable;
+import org.fao.riv.tests.utils.WebTestUtil;
+
+public class InputProjectNig extends WebTestUtil {
+	 @BeforeClass 
+	 public static void start() {      
+	       System.out.println("     test InputProjectNig");
+	 }
+	
+	 @Before
+	 public void deleteExisting() {
+		login();
+		deletePros(true, false, false);
+		deletePros(true, false, true); 
+	 }
+	 
+	 @After
+	    public void close() {
+			clickLink("logoff");
+			closeBrowser();
+	    }
+	 
+	@Test
+	public void createProject() throws Exception {
+		String attachTitle = getMessage("ruralInvest")+" :: "+getMessage("attach.new");
+		String resultsTitle = getMessage("ruralInvest")+" :: "+getMessage("search.searchResults");
+		String blockTitle = getMessage("ruralInvest")+" :: "+getMessage("projectActivity.name");
+		String[] titles = new String[13];
+		for (int i=0;i<13;i++) {
+			titles[i]= 
+					i==3 || i==8 || i==9 || i==10 ? getMessage("ruralInvest")+" :: "+getMessage("project.step"+(i+1)+".nongen") :
+					getMessage("ruralInvest")+" :: "+getMessage("project.step"+(i+1));
+		}
+
+		goHome();
+		
+		getTestContext().setResourceBundleName("dataentry/projectNig");
+		clickLink("newNigProject");
+		assertTitleEquals(titles[0]);
+		
+		// should not be able to attach file yet
+		assertLinkNotPresent("attachFile");
+				
+		// input data
+		String projName = getMessage("step1.projectName");
+		setTextField("projectName", projName);
+		setTextField("userCode", getMessage("step1.userCode"));
+		setTextField("exchRate", getMessage("step1.exchRate"));
+		setTextField("duration", getMessage("step1.duration"));
+		setTextField("location1", getMessage("step1.location1"));
+		setTextField("location2", getMessage("step1.location2"));
+		setTextField("location3", getMessage("step1.location3"));
+		
+		//TODO: set non-default checkbox and select values
+		rivSubmitForm();
+		assertTitleEquals(titles[1]);
+
+		// ATTACH EXTERNAL FILE
+		testAttachFile(titles[0], attachTitle);
+		
+		// STEP 2
+		clickLink("step2");
+		assertTitleEquals(titles[1]);
+		setTextField("benefName",getMessage("step2.benefName"));
+		setTextField("beneDirectMen",getMessage("step2.beneDirectMen"));
+		setTextField("beneDirectWomen", getMessage("step2.beneDirectWomen"));
+		setTextField("beneDirectChild", getMessage("step2.beneDirectChild"));
+		// TODO: test autocalc on benefDirectTotal
+		setTextField("beneDirectNum",getMessage("step2.beneDirectNum"));
+		setTextField("beneIndirectMen",getMessage("step2.beneIndirectMen"));
+		setTextField("beneIndirectWomen",getMessage("step2.beneIndirectWomen"));
+		setTextField("beneIndirectChild",getMessage("step2.beneIndirectChild"));
+		// TODO: test autocalc on benefDirectTotal
+		setTextField("beneIndirectNum",getMessage("step2.beneIndirectNum"));
+		setTextField("benefDesc",getMessage("step2.benefDesc"));
+		rivSubmitForm();
+		assertTitleEquals(titles[2]);
+		
+		// STEP 3
+		setTextField("justification",getMessage("step3.justification"));
+		setTextField("projDesc",getMessage("step3.projDesc"));
+		setTextField("activities",getMessage("step3.activities"));
+		rivSubmitForm();
+		assertTitleEquals(titles[3]);
+		
+		// STEP 4
+		setTextField("technology",getMessage("step4.technology"));
+		setTextField("requirements",getMessage("step4.requirements"));
+		rivSubmitForm();
+		assertTitleEquals(titles[4]);
+		
+		// STEP 5
+		setTextField("sustainability",getMessage("step5.sustainability"));
+		setTextField("enviroImpact",getMessage("step5.enviroImpact"));
+		setTextField("market",getMessage("step5.market"));
+		rivSubmitForm();
+		assertTitleEquals(titles[5]);
+
+		// STEP 6
+		setTextField("organization",getMessage("step6.organization"));
+		setTextField("assumptions",getMessage("step6.assumptions"));
+		rivSubmitForm();
+		assertTitleEquals(titles[6]);
+		
+		// STEP 7
+		
+		// TODO: download excel template and check that it's correct
+		// assets
+		TestTable tt = new TestTable("assetsTable", "step7.asset.", "newAsset", true, new Callable<Void>() {public Void call() { rivSubmitForm(); return null;}});
+		tt.addParam("description").addParam("unitType").addParam("unitNum").addParam("unitCost");
+		tt.addParam("total", InputParamType.TEXT, true).addParam("ownResources").addParam("donated").addParam("financed", InputParamType.TEXT, true);
+		tt.addParam("econLife").addParam("maintCost").addParam("salvage").addParam("replace", InputParamType.CHECKBOX, false).addParam("yearBegin")
+		.addParam("linked", InputParamType.LINKED, false)
+		.addBlanks(5);
+		tt.testWithInput();
+		
+		// labour
+		tt = new TestTable("LabourTable", "step7.labour.", "newLabour", true, new Callable<Void>() {public Void call() { rivSubmitForm(); return null;}})
+		.addParam("description").addParam("unitType", InputParamType.SELECT, false).addParam("unitNum").addParam("unitCost")
+		.addParam("total", InputParamType.TEXT, true).addParam("ownResources").addParam("donated").addParam("financed", InputParamType.TEXT, true).addParam("yearBegin")
+		.addParam("linked", InputParamType.LINKED, false)
+		.addBlanks(5);
+		tt.testWithInput();
+		
+		// service
+		tt = new TestTable("ServicesTable", "step7.service.", "newService", true, new Callable<Void>() {public Void call() { rivSubmitForm(); return null;}})
+		.addParam("description").addParam("unitType").addParam("unitNum").addParam("unitCost")
+		.addParam("total", InputParamType.TEXT, true).addParam("ownResources").addParam("donated").addParam("financed", InputParamType.TEXT, true).addParam("yearBegin")
+		.addParam("linked", InputParamType.LINKED, false)
+		.addBlanks(5);
+		tt.testWithInput();
+		
+		rivSubmitForm();
+		assertTitleEquals(titles[7]);
+		
+		// STEP 8
+		// TODO: download excel template and check
+		// TODO: With and without scenario
+		tt = new TestTable("inputTable", "step8.input", "addMaterial", true, new Callable<Void>() {public Void call() { rivSubmitForm(); return null;}})
+		.addParam("description").addParam("unitType").addParam("unitNum").addParam("unitCost")
+		.addParam("total", InputParamType.TEXT, true)
+		.addParam("statePublic").addParam("other1").addParam("ownResource", InputParamType.TEXT, true)
+		.addParam("linked", InputParamType.LINKED, false)
+		.addBlanks(5);
+		tt.testWithInput();
+		
+		tt = new TestTable("labourTable", "step8.labour", "addLabour", true, new Callable<Void>() {public Void call() { rivSubmitForm(); return null;}})
+		.addParam("description").addParam("unitType", InputParamType.SELECT, false).addParam("unitNum").addParam("unitCost")
+		.addParam("total", InputParamType.TEXT, true)
+		.addParam("statePublic").addParam("other1").addParam("ownResource", InputParamType.TEXT, true)
+		.addParam("linked", InputParamType.LINKED, false)
+		.addBlanks(5);
+		tt.testWithInput();
+		
+		tt = new TestTable("generalTable", "step8.general", "addMaintenance", true, new Callable<Void>() {public Void call() { rivSubmitForm(); return null;}})
+		.addParam("description").addParam("unitType").addParam("unitNum").addParam("unitCost")
+		.addParam("total", InputParamType.TEXT, true)
+		.addParam("statePublic").addParam("other1").addParam("ownResource", InputParamType.TEXT, true)
+		.addParam("linked", InputParamType.LINKED, false)
+		.addBlanks(5);
+		tt.testWithInput();
+		
+		rivSubmitForm();
+		assertTitleEquals(titles[8]);
+		
+		// STEP 9
+		//TODO: download excel template and check
+		//TODO: test clone product
+		//TODO: test delete product
+		
+		// add blocks
+		int i=1;
+		boolean nextItem=true;
+		while (nextItem) {
+			clickLink("addBlock");
+			assertTitleEquals(blockTitle);
+			setTextField("description", getMessage("step9.block."+i+".description"));
+			setTextField("unitType", getMessage("step9.block."+i+".unitType"));
+			setTextField("cycleLength", getMessage("step9.block."+i+".cycleLength"));
+			selectOption("lengthUnit", getMessage("step9.block."+i+".lengthUnit"));
+			setTextField("cyclePerYear", getMessage("step9.block."+i+".cyclePerYear"));
+			
+			// production pattern 
+			for (int x=1; x<=20; x++) { 
+				String value = getMessage("step9.block."+i+".pat"+x);
+				setTextField("pat"+x,value);
+				assertTextFieldEquals("pat"+x, value);
+			}
+			
+			rivSubmitForm();
+			assertTitleEquals(titles[8]);
+			
+			// income
+			tt = new TestTable("incomeTable"+(i-1), "step9.block"+i+".income", "newIncome"+(i-1), true, new Callable<Void>() {public Void call() { rivSubmitForm(); return null;}})
+			.addParam("description").addParam("unitType")
+			.addParam("unitNum").addParam("unitCost")
+			.addParam("total", InputParamType.TEXT, true)
+			.addParam("linked", InputParamType.LINKED, false)
+			.addBlanks(5);
+			tt.testWithInput();
+			
+			// input
+			tt = new TestTable("inputTable"+(i-1), "step9.block"+i+".input", "newInput"+(i-1), true, new Callable<Void>() {public Void call() { rivSubmitForm(); return null;}})
+			.addParam("description").addParam("unitType").addParam("unitNum").addParam("unitCost").addParam("qtyIntern").addParam("extern", InputParamType.TEXT, true)
+			.addParam("transport").addParam("total", InputParamType.TEXT, true).addParam("totalCash", InputParamType.TEXT, true)
+			.addParam("linked", InputParamType.LINKED, false)
+			.addBlanks(5);
+			tt.testWithInput();
+			
+			// labour
+			tt = new TestTable("labourTable"+(i-1), "step9.block"+i+".labour", "newLabour"+(i-1), true, new Callable<Void>() {public Void call() { rivSubmitForm(); return null;}})
+			.addParam("description").addParam("unitType", InputParamType.SELECT, false)
+			.addParam("unitNum")
+			.addParam("unitCost").addParam("qtyIntern").addParam("extern", InputParamType.TEXT, true)
+			.addParam("total", InputParamType.TEXT, true).addParam("totalCash", InputParamType.TEXT, true)
+			.addParam("linked", InputParamType.LINKED, false)
+			.addBlanks(5);
+			tt.testWithInput();
+			
+			//TODO: Test Excel download
+			
+			i++;
+			try {
+				getMessage("step9.block."+i+".withoutProject");
+			} catch (Exception e) {
+				nextItem=false;
+			}
+		}
+		rivSubmitForm();
+		assertTitleEquals(titles[9]);
+		
+		
+		// STEP 10
+		tt = new TestTable("contributionTable", "step10.contribution", "newContrib", true, new Callable<Void>() {public Void call() { rivSubmitForm(); return null;}});
+		tt.addParam("description")
+		.addParam("contribType", InputParamType.SELECT, false)
+		.addParam("unitType")
+		.addParam("unitNum").addParam("unitCost");
+		tt.addParam("total", InputParamType.TEXT, true)
+		.addBlanks(5);
+		tt.testWithInput();
+		
+		rivSubmitForm();
+		assertTitleEquals(titles[10]);
+		
+		// STEP 11
+		// reference income
+		tt = new TestTable("IncomeTable", "step11.income.", "addIncome", false, new Callable<Void>() {public Void call() { rivSubmitForm(); return null;}})
+		.addParam("description").addParam("unitType").addParam("unitCost").addParam("transport")
+		.addBlanks(4);
+		tt.testOutput();
+		
+		// reference cost table
+		tt = new TestTable("GoodsTable", "step11.input.", "addInput", false, new Callable<Void>() {public Void call() { rivSubmitForm(); return null;}})
+		.addParam("description").addParam("unitType").addParam("unitCost").addParam("transport")
+		.addBlanks(4);
+		tt.testOutput();
+		
+		// reference labour table
+		tt = new TestTable("LabourTable", "step11.labour.", "addLabour", false, new Callable<Void>() {public Void call() { rivSubmitForm(); return null;}})
+		.addParam("description").addParam("unitType").addParam("unitCost")
+		.addBlanks(4);
+		tt.testOutput();
+		rivSubmitForm();
+		assertTitleEquals(titles[11]);
+		
+		// STEP 12
+		clickRadioOption("reccCode", getMessage("step12.reccCode"));
+		setTextField("reccDate", getMessage("step12.reccDate"));
+		setTextField("reccDesc", getMessage("step12.reccDesc"));
+		rivSubmitForm();
+		assertTitleEquals(titles[12]);
+		
+		// STEP 13 
+		rivSubmitForm();
+		assertTitleEquals(resultsTitle);
+		
+		//Check new project exists in results table
+		assertTableRowCountEquals("results", 6);
+		assertTextInTable("results", projName);
+	
+		// CLONE PROJECT
+		assertLinkPresentWithImage("edit.png");
+		clickLinkWithImage("edit.png");
+		assertTitleEquals(titles[0]);
+		clickLinkWithImage("duplicate.gif");
+		assertTitleEquals(titles[0]);
+		assertImagePresentPartial("locked.gif", null);
+		
+		// step 1
+		verifyProjectNig("dataentry/projectNig");
+		
+		//Check new project exists in results table
+		assertTableRowCountEquals("results", 7);
+		assertTextInTable("results", projName);
+	}
+}
---
+++ RIV4-tests/src/test/java/org/fao/riv/tests/utils/InputParam.java
@@ -0,0 +1,96 @@
+package org.fao.riv.tests.utils;
+
+public class InputParam {
+	private String name;
+	private String value;
+	private InputParamType paramType;
+	private boolean calculated;
+	private String[] options;
+	//private String displayValue;
+//	private String[] selectOptions;
+//	private String[] selectValues;
+	
+	// constructors
+	public InputParam(String name) {
+		this.setName(name);
+		this.setParamType(InputParamType.TEXT);
+	}
+	
+	public InputParam(String name, InputParamType paramType, boolean calculated) {
+		this.setName(name);
+		this.setParamType(paramType);
+		this.setCalculated(calculated);
+	}
+	
+	protected InputParam() { } // for subclasses
+	
+	// fields
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setParamType(InputParamType paramType) {
+		this.paramType = paramType;
+	}
+
+	public InputParamType getParamType() {
+		return paramType;
+	}
+
+	public void setCalculated(boolean calculated) {
+		this.calculated = calculated;
+	}
+
+	public boolean isCalculated() {
+		return calculated;
+	}
+	
+//	public String getDisplayValue() {
+//		String displayValue="";
+//		
+//		for (int i=0; i<selectValues.length; i++)
+//			if (selectValues[i].equals(value)) 
+//				displayValue=selectOptions[i];
+//		
+//		return displayValue;
+//	}
+
+//	public void setSelectOptions(String[] selectOptions) {
+//		this.selectOptions = selectOptions;
+//	}
+//
+//	public String[] getSelectOptions() {
+//		return selectOptions;
+//	}
+//
+//	public void setSelectValues(String[] selectValues) {
+//		this.selectValues = selectValues;
+//	}
+//
+//	public String[] getSelectValues() {
+//		return selectValues;
+//	}
+
+	public String[] getOptions() {
+		return options;
+	}
+
+	public void setOptions(String[] options) {
+		this.options = options;
+	}
+
+	public enum InputParamType { TEXT, SELECT, CHECKBOX, HIDDEN, FILE, NONE, LINKED }
+	
+}
