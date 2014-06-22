453839ce1aada98dd41d3bd3ba445d9253122270
Bar Zecharya <bar.zecharya@fao.org>
2014-06-22 07:06:27 +0000

    add export generic test to test suite
    git-svn-id: https://svn.fao.org/projects/ruralinvest/RIV4/trunk@66805 d7fa552c-c32d-11e1-9f2d-6d39f2a56844

--- RIV4-tests/src/test/java/org/fao/riv/tests/download/DownloadSuite.java
+++ RIV4-tests/src/test/java/org/fao/riv/tests/download/DownloadSuite.java
@@ -8,7 +8,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 
 @RunWith(value = Suite.class)
-@SuiteClasses(value = { ProfilePdf.class, ProjectPdf.class, Error.class, Settings.class })
+@SuiteClasses(value = { ProfilePdf.class, ProjectPdf.class, Error.class, Settings.class, ExportGeneric.class })
 public class DownloadSuite {
 	
 	 @BeforeClass 
