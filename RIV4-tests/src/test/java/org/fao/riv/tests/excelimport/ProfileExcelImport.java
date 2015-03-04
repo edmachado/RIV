1bf54c1f941110f43bd47229df273602d7b68542
Bar Zecharya <bar.zecharya@fao.org>
2015-03-04 17:51:26 +0000

    excel import for profile "without project" scenario
    git-svn-id: https://svn.fao.org/projects/ruralinvest/RIV4/trunk@67104 d7fa552c-c32d-11e1-9f2d-6d39f2a56844

--- RIV4-tests/src/test/java/org/fao/riv/tests/excelimport/ProfileExcelImport.java
+++ RIV4-tests/src/test/java/org/fao/riv/tests/excelimport/ProfileExcelImport.java
@@ -53,23 +53,17 @@ public class ProfileExcelImport extends WebTestUtil {
 	public void IgExcelImportDataError() throws Exception {
 		deletePros(false, true);
 		
-		String[] titles = new String[9];
-		for (int i=0;i<9;i++) {
-			titles[i]= getMessage("ruralInvest")+" :: "+getMessage("profile.step"+(i+1));
-		}
+		String[] titles = profileStepTitles(true);
 
 		clickLink("goHome");
 		
 		// import file
 		clickLink("importProfileIg");
-		importFile(ImportFile.ProfileIgV20.getFile());
+		importFile(ImportFile.ProfileIgV42.getFile());
 		
 		// go through all steps
 		for (int i=0; i<3; i++) {
 			assertTitleEquals(titles[i]);
-			if (i==0) {
-				setTextField("benefFamilies", "12");
-			}
 			rivSubmitForm();
 		}
 		
@@ -80,15 +74,107 @@ public class ProfileExcelImport extends WebTestUtil {
 		setTextField("qqfile", ImportFile.ProfileXlsInvestErrorData.getFile().getAbsolutePath());
 		assertTextPresent(getMessage("import.excel.error.datatype"));
 	}
+	
+	@Test
+	public void IgExcelImportWithout() throws Exception {
+		deletePros(false, true);
+		
+		String[] titles = profileStepTitles(true);
+		getTestContext().setResourceBundleName("dataentry/profileIg");
+
+		clickLink("goHome");
+		
+		// import file
+		clickLink("importProfileIg");
+		importFile(ImportFile.ProfileIgV42.getFile());
+		
+		// go through all steps
+		for (int i=0; i<3; i++) {
+			assertTitleEquals(titles[i]);
+			rivSubmitForm();
+		}
+		
+		String url;
+		
+		// STEP 4
+		url=getTestingEngine().getPageURL().toString();
+		clickLink("downloadTemplate");
+		File fTemplate = folder.newFile("invest-template.xlsx");
+		saveAs(fTemplate);
+		gotoPage(url);
+		assertTitleEquals(titles[3]);
+		// re-upload template to zero tables
+		clickLink("importExcel");
+		setTextField("qqfile", fTemplate.getAbsolutePath());
+		gotoPage(url);
+		assertTitleEquals(titles[3]);
+		assertTableNotPresent("goodsListTable");
+		assertTableNotPresent("labourListTable");
+		
+		// import file
+		clickLink("importExcel");
+		setTextField("qqfile", ImportFile.ProfileXlsInvest.getFile().getAbsolutePath());
+		gotoPage(url);
+		verifyProfileTablesStep4();
+		
+		// export and import again
+		clickLink("downloadExcel");
+		File f = folder.newFile("step4.xlsx"); 
+		saveAs(f);
+		gotoPage(url);
+		clickLink("importExcel");
+		setTextField("qqfile", f.getAbsolutePath());
+		gotoPage(url);
+		verifyProfileTablesStep4();
+		
+		rivSubmitForm();
+		assertTitleEquals(titles[4]);
+		
+		// STEP 5
+		// import file
+		url = getTestingEngine().getPageURL().toString();
+
+		// download template
+		clickLink("downloadTemplate");
+		fTemplate = folder.newFile("general-template.xlsx");
+		saveAs(fTemplate);
+		gotoPage(url);
+		assertTitleEquals(titles[4]);
+		
+		// re-upload template to zero tables
+		clickLink("importExcel");
+		setTextField("qqfile", fTemplate.getAbsolutePath());
+		gotoPage(url);
+		assertTitleEquals(titles[4]);
+		assertTableNotPresent("generalTable");
+		assertTableNotPresent("generalTableWo");
+		
+		clickLink("importExcel");
+		setTextField("qqfile", ImportFile.ProfileXlsGeneral.getFile().getAbsolutePath());
+		gotoPage(url);
+		verifyProfileTablesStep5();
+		
+		// export and import again
+		clickLink("downloadExcel");
+		File f2 = folder.newFile("step5.xlsx"); 
+		saveAs(f2);
+		gotoPage(url);
+		clickLink("importExcel");
+		setTextField("qqfile", f2.getAbsolutePath());
+		gotoPage(url);
+		verifyProfileTablesStep5();
+				
+		rivSubmitForm();
+		assertTitleEquals(titles[5]);
+		
+    	getTestContext().setResourceBundleName("messages/messages");
+	}
 
 	@Test
 	public void IgExcelImport() throws Exception {
 		deletePros(false, true);
 		
-		String[] titles = new String[9];
-		for (int i=0;i<9;i++) {
-			titles[i]= getMessage("ruralInvest")+" :: "+getMessage("profile.step"+(i+1));
-		}
+		String[] titles = profileStepTitles(true);
 		getTestContext().setResourceBundleName("dataentry/profileIg");
 
 		clickLink("goHome");
