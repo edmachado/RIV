111fc3e8c155036ae36de9f9592cc3a042a4aa19
Bar Zecharya <bar.zecharya@fao.org>
2015-04-24 09:50:20 +0000

    not specified donor for nig projects; include missed tests in suites
    git-svn-id: https://svn.fao.org/projects/ruralinvest/RIV4/trunk@67197 d7fa552c-c32d-11e1-9f2d-6d39f2a56844

--- RIV4-tests/src/test/java/org/fao/riv/tests/exceldownload/ExcelDownloadSuite.java
+++ RIV4-tests/src/test/java/org/fao/riv/tests/exceldownload/ExcelDownloadSuite.java
@@ -8,7 +8,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 
 @RunWith(value = Suite.class)
-@SuiteClasses(value = { ProfileXls.class, ProjectXls.class, SearchResultsReports.class, ProjectTemplates.class, ProfileTemplates.class })
+@SuiteClasses(value = { ProfileXls.class, ProjectXls.class, SearchResultsReports.class, ProjectTemplates.class, ProfileTemplates.class, ProjectReportValues.class })
 public class ExcelDownloadSuite {
 	
 	 @BeforeClass 
