5f9e92189594c6057d472e7e4940f1434d10689e
Davide Fiorentino <davide.fiorentino@fao.org>
2015-10-30 08:12:16 +0000

    update setup for static silent installation
    providing some message for users.
    
    git-svn-id: https://svn.fao.org/projects/ruralinvest/RIV4/trunk@67295 d7fa552c-c32d-11e1-9f2d-6d39f2a56844

--- RIV4-installer/src/izpack/CD/setup.bat
+++ RIV4-installer/src/izpack/CD/setup.bat
@@ -1,4 +1,11 @@
 @echo off
-echo %JAVA_HOME% | Findstr/I "."
-IF ERRORLEVEL 1 jre\jre.exe INSTALLCFG=setup.ini
-%JAVA_HOME%\jre\bin\java.exe -cp lib fao.JavaDiscovery
+configure
+echo Please wait while FAO RuralInvest collects your settings...
+set RIV_JAVA=java
+java -version > nul 2> nul
+IF %ERRORLEVEL% NEQ 0 (
+    echo Installing missing required component. It could take some minutes. Please wait
+    jre\jre.exe INSTALL_SILENT=1 STATIC=1 INSTALLDIR="%PROGRAMFILES(X86)%\java99"
+    set RIV_JAVA="%PROGRAMFILES(X86)%\java99\bin\java"
+)
+%RIV_JAVA% -jar %CD%\lib\riv.jar
