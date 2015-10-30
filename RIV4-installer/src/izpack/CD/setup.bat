40df66e29de01c91bf7fc4cc862c89abb38b71f9
Davide Fiorentino <davide.fiorentino@fao.org>
2015-10-30 08:14:44 +0000

    update waiting message in setup.bat
    git-svn-id: https://svn.fao.org/projects/ruralinvest/RIV4/trunk@67298 d7fa552c-c32d-11e1-9f2d-6d39f2a56844

--- RIV4-installer/src/izpack/CD/setup.bat
+++ RIV4-installer/src/izpack/CD/setup.bat
@@ -4,7 +4,7 @@ echo Please wait while FAO RuralInvest collects your settings...
 set RIV_JAVA=java
 java -version > nul 2> nul
 IF %ERRORLEVEL% NEQ 0 (
-    echo Installing missing required component. It could take some minutes. Please wait
+    echo Installing missing required component. It could take a few minutes. Please wait.
     jre\jre.exe INSTALL_SILENT=1 STATIC=1 INSTALLDIR="%PROGRAMFILES(X86)%\java99"
     set RIV_JAVA="%PROGRAMFILES(X86)%\java99\bin\java"
 )
