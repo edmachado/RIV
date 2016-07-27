0525a9b2ed370801cae7800e9bcc4156ea847401
Bar Zecharya <bar.zecharya@fao.org>
2016-07-27 18:57:17 +0000

    per-year contributions XLS import
    git-svn-id: https://svn.fao.org/projects/ruralinvest/RIV4/trunk@67608 d7fa552c-c32d-11e1-9f2d-6d39f2a56844

--- RuralInvest4/src/main/java/riv/web/controller/ExcelImportController.java
+++ RuralInvest4/src/main/java/riv/web/controller/ExcelImportController.java
@@ -175,7 +175,8 @@ public class ExcelImportController {
 		ProjectItemValidator validator = new ProjectItemValidator();
 		Project p = dataService.getProject(id, 10);
 		validator.setIncomeGen(false);
-		validator.setDuration(p.getDuration()); 
+		int yearsInReport=p.isPerYearContributions()?p.getDuration():1;
+		validator.setDuration(yearsInReport); 
 		
 		Map<String, Integer> donorsByOrder = new HashMap<String, Integer>();
 		for (Donor d : p.getDonors()) {
@@ -185,29 +186,16 @@ public class ExcelImportController {
 			else { desc = d.getDescription(); }
 			donorsByOrder.put(desc, d.getOrderBy());
 		}
+	
+		XlsImportTable<ProjectItemContribution> table = new XlsImportTable<ProjectItemContribution>(ProjectItemContribution.class, 0, 5, validator)
+				.addColumn(0, "description", false)
+				.addSelectColumn(1, "donorOrderBy", donorsByOrder)
+				.addColumn(2, "unitType", false)
+				.addColumn(3, "unitCost", true)
+				.addPerYearColumns(4, yearsInReport, "unitNum");
+		List<ProjectItemContribution> items = table.readTable(workbook.getSheetAt(0), messageSource);
 		
-//		List<ProjectItemContribution> items = new ArrayList<ProjectItemContribution>();
-//		XlsImportTable<ProjectItemContribution> table = new XlsImportTable<ProjectItemContribution>(ProjectItemContribution.class, 0, 5, validator)
-//				.addColumn(0, "description", false)
-//				.addSelectColumn(1, "donorOrderBy", donorsByOrder)
-//				.addColumn(2, "unitType", false)
-//				.addColumn(3, "unitNum", true)
-//				.addColumn(4, "unitCost", true);
-		
-		
-//		for (int year=1; year<=p.getDuration();year++) {
-//			if (p.isPerYearContributions() || year==1) {
-//				table.setStartRow(2+items.size()+(year-1)*4);
-//				List<ProjectItemContribution> yearItems = table.readTable(workbook.getSheetAt(0), messageSource);
-//				for (ProjectItemContribution i : yearItems) {
-// 					i.setYear(year);
-//				}
-//				items.addAll(yearItems);
-//				yearItems.clear();
-//			}
-//		}
-		
-//		dataService.replaceProjectContribution(id, items);
+		dataService.replaceProjectContribution(id, items);
 	}
 	
 	private void importProjectGeneralNongen(int id, InputStream file) throws ExcelImportException {
