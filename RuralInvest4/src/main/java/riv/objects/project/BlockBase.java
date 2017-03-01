376674e21fb2255dccb3fc713c1cd41b2cf20efa
Bar Zecharya <bar.zecharya@fao.org>
2017-03-01 12:40:46 +0000

    saving properties file on block
    git-svn-id: https://svn.fao.org/projects/ruralinvest/RIV4/trunk@67717 d7fa552c-c32d-11e1-9f2d-6d39f2a56844

--- RuralInvest4/src/main/java/riv/objects/project/BlockBase.java
+++ RuralInvest4/src/main/java/riv/objects/project/BlockBase.java
@@ -152,8 +152,10 @@ public abstract class BlockBase implements ProductOrBlock, Serializable, OrderBy
 		   sb.append(base+"cycleLength="+rc.getSetting().getDecimalFormat().format(this.getCycleLength())+lineSeparator);
 		   sb.append(base+"lengthUnit="+rc.getLengthUnits().get(this.getLengthUnit())+lineSeparator);
 		   sb.append(base+"cyclePerYear="+rc.getSetting().getDecimalFormat().format(this.getCyclePerYear())+lineSeparator);
-		   sb.append(base+"cycleFirstYear="+rc.getSetting().getDecimalFormat().format(this.getCycleFirstYear())+lineSeparator);
-		   sb.append(base+"cycleFirstYearIncome="+rc.getSetting().getDecimalFormat().format(this.getCycleFirstYearIncome())+lineSeparator);
+		   if (this.getProject().getIncomeGen()) {
+				   sb.append(base+"cycleFirstYear="+rc.getSetting().getDecimalFormat().format(this.getCycleFirstYear())+lineSeparator);
+				   sb.append(base+"cycleFirstYearIncome="+rc.getSetting().getDecimalFormat().format(this.getCycleFirstYearIncome())+lineSeparator);
+		   }
 		   sb.append(base+"cycles="+cycles+lineSeparator);
 		   
 		   for (int type=0;type<3;type++) {
