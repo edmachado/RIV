f760d548b4c5f0f3788efda2658293cd247c7aa1
Bar Zecharya <bar.zecharya@fao.org>
2018-07-16 10:10:29 +0200

    jvm value in registry

---
+++ izpack-includes/src/main/java/org/fao/riv/installer/actions/UpdateRegistryJavaHomeAction.java
@@ -0,0 +1,22 @@
+package org.fao.riv.installer.actions;
+
+import org.fao.riv.installer.util.WinRegistry;
+
+import com.izforge.izpack.panels.process.AbstractUIProcessHandler;
+
+public class UpdateRegistryJavaHomeAction  {
+
+    private static final String KEYPATH = "Software\\Apache Software Foundation\\Procrun 2.0\\RuralInvest4\\Parameters\\Java";
+
+    public void run (AbstractUIProcessHandler uih, String[] args) {
+        String jvm = args[0]+"\\bin\\client\\jvm.dll";
+    	
+        try {
+        	WinRegistry.writeStringValue(WinRegistry.HKEY_LOCAL_MACHINE, KEYPATH, "Jvm", jvm);
+        } catch (Exception e) {
+        	System.out.println(e);
+            e.printStackTrace();
+        }
+    }
+
+}
